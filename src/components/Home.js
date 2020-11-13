cb168446bf100997403e5488114375f5bdee9925
mango-jam-0513 <mangojamstar0513@outlook.com>
2020-11-13 22:17:12 +0800

    update-new

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, {useRef, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 
-function Home() {
+function Home(props) {
     const h1Ref = useRef()
 
     useEffect(() => {
