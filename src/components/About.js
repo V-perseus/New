7c096b795115a8f37a0c72c4fd6e977af250f530
mango-jam-0513 <mangojamstar0513@outlook.com>
2020-11-13 22:08:54 +0800

    update

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            Mango Jam
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, {useRef, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+
+function Home() {
+    const h1Ref = useRef()
+
+    useEffect(() => {
+
+    })
+    return (
+        <div>
+            <h1 ref={h1Ref}>
+                Home. Go to <Link to="/about">About</Link> 
+            </h1>
+        </div>
+    )
+};
+
+
+export default Home
